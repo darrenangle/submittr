0c70a8f222ae9e6ee5c5c13cf005946820c0b765
Darren Angle <darrendarrenangle@gmail.com>
2013-04-30 11:32:52 -0500

    js file organization, views as classes, pre-demo view

--- js/Submittr.js
+++ js/Submittr.js
@@ -1,6 +1,28 @@
+//TRY THE DEMO BUTTON CONVERTS THE PAGE INTO A FAKE MAGAZINE!!!!
+
 //if user is logged in:
 // - display logout button
 // - display profile link
 // - display current submissions
 
 //user upload form includes profile information 
+// classes as views? 
+// pre-demo view, demo-login view, demo-logged-in view
+
+$(document).ready(function(){ 
+						   
+	
+	
+	var 
+		//views
+		pdv = $(".pre-demo"), 
+
+		//objects
+		demobutton = $("#d-btn");
+
+
+	demobutton.click(function(){
+		pdv.fadeOut(100);
+	});
+});
+

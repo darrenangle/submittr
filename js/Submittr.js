79b5c1d4236c90a4b0fb32a7161cb3fe8002389e
Darren Angle <darrendarrenangle@gmail.com>
2013-05-20 21:15:37 -0500

    Parse initialization for demo, login/signup underscore templating

---
+++ js/Submittr.js
@@ -0,0 +1,22 @@
+$(function(){
+
+	Parse.$ = jQuery;
+
+	Parse.initialize("QbnWiSsZXhRgvcw7gLJFE9XMX9l024X5MXmhOexJ", 
+					"cmiAs3yoIeOxPl1HEOaLfNXMV7I6TaFtKe2QaVbl");
+
+
+
+
+	//Views
+
+	//Main view for Submittr App
+	var SubmittrView = Parse.View.extend({
+
+		el:
+
+
+	});
+
+
+})
